b551f6ad888562eff34824b27610e4925b0b16b9
Vladimir Frants <franz@a.local>
2022-05-17 17:13:58 -0400

    double resize

--- utils.py
+++ utils.py
@@ -23,7 +23,7 @@ def calc_psnr(im1, im2):
     im1_y = cv2.cvtColor(im1, cv2.COLOR_BGR2YCR_CB)[:, :, 0]
     im2_y = cv2.cvtColor(im2, cv2.COLOR_BGR2YCR_CB)[:, :, 0]
 
-    h, w, _ = im2_y.shape
+    h, w = im2_y.shape
     im1_y = cv2.resize(im1_y, (w, h))
 
     ans = [peak_signal_noise_ratio(im1_y, im2_y)]
@@ -35,6 +35,8 @@ def calc_ssim(im1, im2):
 
     im1_y = cv2.cvtColor(im1, cv2.COLOR_BGR2YCR_CB)[:, :, 0]
     im2_y = cv2.cvtColor(im2, cv2.COLOR_BGR2YCR_CB)[:, :, 0]
+    h, w = im2_y.shape
+    im1_y = cv2.resize(im1_y, (w, h))
     ans = [structural_similarity(im1_y, im2_y)]
     return ans
 
